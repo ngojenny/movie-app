fff34af6cf572122c80c95a148469d331b51cd04
Jenny Ngo <jennyngo.dev@gmail.com>
2016-05-27 17:04:35 -0400

    sent ajax calls for trailer

--- public/js/main.js
+++ public/js/main.js
@@ -92,11 +92,13 @@ app.sortArray = function (combinedPgsArray) {
 };
 
 //this function will pull the top ten/first ten objects in array
+
 app.topTen = function (topMovies) {
-	// console.log(topMovies.slice(0,10));
+	console.log(topMovies.slice(0, 10));
 	var topTenMovies = topMovies.slice(0, 10);
-	console.log("top movies", topTenMovies);
+
 	app.displayTopTen(topTenMovies);
+	app.getIdArray(topTenMovies);
 };
 
 //this function will pull out the poster_path from the 10 objects within the array.
@@ -139,6 +141,36 @@ app.displayMoreInfo = function (singleMovie) {
 	});
 };
 
+//This function will create an array of movie ids
+app.getIdArray = function (movieArray) {
+	console.log(movieArray);
+	var idArray = [];
+	movieArray.forEach(function (movieObject) {
+		idArray.push(movieObject.id);
+	});
+	app.getTrailers(idArray);
+	console.log(idArray);
+};
+
+app.getTrailers = function (idArray) {
+	var getTrailer = idArray.map(function (id) {
+		return $.ajax({
+			url: 'http://api.themoviedb.org/3/movie/' + id + '/videos',
+			method: 'GET',
+			dataType: 'jsonp',
+			data: {
+				api_key: 'f43968b7420dc8dd5dc5be75cb2d3725'
+			}
+		});
+	});
+
+	$.when.apply(null, getTrailer).then(function (res) {
+		console.log(res);
+		console.log('hello?');
+	});
+};
+
 $(document).ready(function () {
+
 	app.init();
 });
